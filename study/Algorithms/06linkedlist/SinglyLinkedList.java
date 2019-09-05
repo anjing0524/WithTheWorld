a4f8d3a16c31f543dbd591d8e84989975d9a01d6
anjing0524 <anjing0524ls@163.com>
2019-09-05 16:57:14 +0800

    fix inverseLinkList

--- study/Algorithms/06linkedlist/SinglyLinkedList.java
+++ study/Algorithms/06linkedlist/SinglyLinkedList.java
@@ -127,7 +127,7 @@ public class SinglyLinkedList{
     public void printAll() {
         Node p = head;
         while (p != null) {
-            System.out.print(p.data + " ");
+            System.out.print(p.data + "=>");
             p = p.next;
         }
         System.out.println();
@@ -152,7 +152,101 @@ public class SinglyLinkedList{
         return false;
     }
 
+    public boolean palindrome(){
+        if(head == null) return false;
+        System.out.println("开始执行找到中间节点");
+        Node fast = head;
+        Node slow = head;
+
+        if(slow.next == null) return true;
+        while(fast.next != null &&  fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        System.out.println("中间节点"+slow.data);
+        Node leftLink = null;
+        Node rightLink = null;
+        if(fast.next == null){
+            System.out.println("奇数");
+            //  slow 一定为整个链表的中点 并且节点数为奇数
+            rightLink = slow.next;
+            leftLink = inverseLinkList(slow).next;
+        }else{
+            System.out.println("偶数");
+            rightLink = slow.next;
+            leftLink = inverseLinkList(slow);
+        }
+        return TFResult(leftLink, rightLink);
+
+    }
+
+    // 一直在头后面插入
+    public Node inverseLinkList_head(Node p){
+        Node Head = new Node(9999,null);
+        Head.next = p;
+        Node cur = p.next;
+        p.next = null;
+        Node next = null;
+        while(cur != null){
+            next = cur.next;
+            cur.next = Head.next;
+            Head.next = cur;
+            System.out.println("first " + Head.data);
+            cur = next;
+        }
+        return Head;
+
+    }
+
+    public Node inverseLinkList(Node p){
+        System.out.println(p.data);
+        System.out.println(head.data);
+        Node pre = null;
+        Node r = head;
+        Node next= null;
+        System.out.println("z---" + r.data);
+        while(r != p){
+            // 额外存储了一下
+            next = r.next;
+            r.next = pre;
+            pre = r;
+            r = next;
+        }
+        r.next = pre;
+        return r;
+    }
     
+    public static void main(String[]args){
+
+        SinglyLinkedList link = new SinglyLinkedList(); 
+        System.out.println("hello");
+        //int data[] = {1};
+        //int data[] = {1,2};
+        //int data[] = {1,2,3,1};
+        //int data[] = {1,2,5};
+        //int data[] = {1,2,2,1};
+       // int data[] = {1,2,5,2,1};
+        int data[] = {1,2,3,2,1};
+
+        for(int i =0; i < data.length; i++){
+            //link.insertToHead(data[i]);
+            link.insertTail(data[i]);
+        }
+       // link.printAll();
+       // Node p = link.inverseLinkList_head(link.head);
+       // while(p != null){
+       //     System.out.println("aa"+p.data);
+       //     p = p.next;
+       // }
+
+        System.out.println("打印原始:");
+        link.printAll();
+        if (link.palindrome()){
+            System.out.println("回文");
+        }else{
+            System.out.println("不是回文");
+        }
+    }
 
 
 
@@ -174,4 +268,6 @@ public class SinglyLinkedList{
     public static Node createNode(int value) {
         return new Node(value, null);
     }
+
+
 }
