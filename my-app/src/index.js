e50dc58f26b4fabe833b49dbb6e186362a2172d4
liushuo <anjing0524ls@163.com>
2020-11-08 19:14:23 +0800

    参考官方示例增加了 todos

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,9 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import rootReducer from './reducers'
+import {configureStore} from '@reduxjs/toolkit'
+import {Provider} from 'react-redux'
+
+const store =configureStore({
+  reducer:rootReducer
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
