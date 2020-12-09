c563b25b2362a302112ca7cae186b248ae959733
liushuo <anjing0524ls@163.com>
2020-12-09 10:23:37 +0800

    index.html

--- c_cpp/hello.c
+++ c_cpp/hello.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
- 
+
 int main()
 {
-	    /* 我的第一个 C 程序 */
-	    printf("Hello, World! \n");
-	     
-	        return 0;
+	/* 我的第一个 C 程序 */
+	printf("Hello, World! \n");
+
+	printf("this is a test ! \n");
+
+	return 0;
 }
